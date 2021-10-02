3fe3657412b6f085f890c8f2515ed0a47980b999
uzbek <tba-93@yandex.uz>
2021-10-03 00:14:58 +0500

    Address unique keys are added (city, street)

--- src/main/java/com/example/appdatarest/entity/Address.java
+++ src/main/java/com/example/appdatarest/entity/Address.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"city", "street"})})
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
